a12551191530b37343536170b214d9973072293e
jpromanonet <programming@jpromano.net>
2022-12-30 17:56:00 -0300

    Players keys

--- player.py
+++ player.py
@@ -42,6 +42,12 @@ class Player:
             self.angle += PLAYER_ROT_SPEED * self.game.delta_time
         self.angle %= math.tau # tau = 2 * pi
 
+    def draw(self):
+        pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
+                     (self.x * 100 + WIDTH * math.cos(self.angle),
+                      self.y * 100 + WIDTH * math.sin(self.angle)), 2)
+        pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
+
     def update(self):
         self.movement()
 
