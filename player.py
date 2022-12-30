a99fdf9caf7df8a0dc87e9c40e96b33ee1597259
jpromanonet <programming@jpromano.net>
2022-12-30 17:27:20 -0300

    Player settings

--- player.py
+++ player.py
@@ -5,3 +5,21 @@ import math
 
 
 class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+
+    def movement(self):
+        pass
+
+    def update(self):
+        self.movement()
+
+    @property
+    def __pos__(self):
+        return self.x, self.y
+
+    @@property
+    def map_os(self):
+        return int(self.x), int(self.y)
