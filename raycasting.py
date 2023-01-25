7fef7e8b642b86c33dd465bff7213109f3ad0175
jpromanonet <programming@jpromano.net>
2023-01-25 15:21:27 -0300

    Ray casting

--- raycasting.py
+++ raycasting.py
@@ -10,3 +10,6 @@ class RayCasting:
 
     def ray_cast(self):
         pass
+
+    def update(self):
+        self.ray_cast()
--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+import math
+
 # Display settings
 RES = WIDTH, HEIGHT = 1600, 900
 FPS = 0
@@ -7,3 +9,10 @@ PLAYER_POS = 1.5, 5 # mini_map
 PLAYER_ANGLE = 0
 PLAYER_SPEED = 0.004
 PLAYER_ROT_SPEED = 0.002
+
+# Ray Casting Settings
+FOV = math.pi
+HALF_FOV = FOV / 2
+NUM_RAYS = WIDTH // 2
+HALF_NUM_RAYS = NUM_RAYS // 2
+DELTA
