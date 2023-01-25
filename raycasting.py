872bf9b3a73d492c1bdc7a409ad898541379aa4a
jpromanonet <programming@jpromano.net>
2023-01-25 15:58:33 -0300

    Ray casting

--- raycasting.py
+++ raycasting.py
@@ -14,6 +14,12 @@ class RayCasting:
 
         ray_angle = self.game.player.angle = HALF_FOV + 0.0001
         for ray in range(NUM_RAYS):
+            sin_a = math.sin(ray_angle)
+            cos_a = math.cos(ray_angle)
+
+            # verticals
+            x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
+
             ray_angle += DELTA_ANGLE
 
     def update(self):
